6b1abcc232012dabae8ae279a837f429aafbbd08
KURO2303 <160729722+KURO2303@users.noreply.github.com>
2024-02-22 11:25:01 +0000

    Number Class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,21 @@
 
 package com.mycompany.app;
 
+public class Number {
+    public int i;
+   }
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
         Number n1 = new Number();
-    Number n2 = new Number();
-    n1.i = 2;
-    n2.i = 5;
-    n1.i = n2.i;
-    System.out.println(n1.i);
-    n2.i = 10;
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1.i = n2.i;
+        System.out.println(n1.i);
+        n2.i = 10;
+
+        //assignment 2
     }
 }
