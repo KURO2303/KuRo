578b73ff17af9c9d15d0662fe570c8d99b458488
KURO2303 <160729722+KURO2303@users.noreply.github.com>
2024-02-22 11:21:17 +0000

    Assignment 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,5 +8,12 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+        Number n1 = new Number();
+    Number n2 = new Number();
+    n1.i = 2;
+    n2.i = 5;
+    n1.i = n2.i;
+    System.out.println(n1.i);
+    n2.i = 10;
     }
 }
