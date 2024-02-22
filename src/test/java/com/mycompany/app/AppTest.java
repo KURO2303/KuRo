a6367a4e9ec62986ad8a28556a23474a646ae73c
KURO2303 <160729722+KURO2303@users.noreply.github.com>
2024-02-22 18:16:13 +0700

    Initial commit

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    public AppTest() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
