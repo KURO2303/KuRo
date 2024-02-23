35ef160906edc0afa098b45b7d1cc3ff539bdac3
KURO2303 <Redragon23030509@gmail.com>
2024-02-23 07:28:03 +0700

    Alias

--- src/main/java/com/mycompany/app/New_Folder/App.java
+++ src/main/java/com/mycompany/app/New_Folder/App.java
@@ -14,8 +14,9 @@ public class App {
         n1.i = 2;
         n2.i = 5;
         n1.i = n2.i;
+        n2.i = 10;	
+        System.out.println(n2.i);
         System.out.println(n1.i);
-        n2.i = 10;
 
         //assignment 2
 
@@ -35,5 +36,7 @@ public class App {
         System.out.println(n1.i);
         System.out.println(n2.i);
         System.out.println("\n");
+
+
     }
 }
