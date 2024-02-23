9d408fabe0e261088a480560dd8e4b0f4eb979d1
KURO2303 <Redragon23030509@gmail.com>
2024-02-23 08:01:59 +0700

    EXERCISE 1

---
+++ src/main/java/com/mycompany/app/New_Folder/EX1.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.New_Folder;
+
+public class EX1 {
+    public static void main(String[] args){
+        int n=100;
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
