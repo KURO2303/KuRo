f4fe427bafeaa2427070f7c3f39f354acec33381
KURO2303 <Redragon23030509@gmail.com>
2024-02-23 10:44:04 +0700

    EXERCISE 2 (redo)

--- src/main/java/com/mycompany/app/New_Folder/EX3.java
+++ src/main/java/com/mycompany/app/New_Folder/EX3.java
@@ -1,12 +1,15 @@
 package com.mycompany.app.New_Folder;
 
 import java.util.Random;
+import java.util.Scanner;
 
-public class EX2 {
+public class EX3 {
     public static void main(String[] args){
-        Random rand = new Random();
-        for(int i=1; i<=25; i++){
-            int randNumber1 = rand.nextInt(100);
+    Scanner Loop = new Scanner(System.in);
+     boolean stop = false;
+     Random rand = new Random();
+     while(!stop){
+            int randNumber1= rand.nextInt(100);
             int randNumber2= rand.nextInt(100);
             if(randNumber1>randNumber2){
                 System.out.println(randNumber1+">"+randNumber2);
@@ -15,6 +18,15 @@ public class EX2 {
             } else {
                 System.out.println("NO VALUE");
             }
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-}
-}
+        Loop.close();
+        System.out.println("End");
+        }
+     }
+
+
