723c90e1a81a37456c54de8420f1e9a512be5c7b
Bùi Minh Đức <160729391+ducbui2005hadd@users.noreply.github.com>
2024-02-24 22:09:20 +0700

    My exercise 8 at Thinking In Java 4th Edition :v

---
+++ Ex8.java
@@ -0,0 +1,19 @@
+package com.mycompany.app.Week1_Code3;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        for (int n = 0 ; n < 3; n++){
+            switch (n) {
+                case 0:
+                     System.out.println("n= " + n);
+                     break;
+                case 1:    
+                     System.out.println("n= " + n);
+                     break;
+                    case 2: 
+                     System.out.println("n= " + n );
+                }
+         }
+    }
+}
+
