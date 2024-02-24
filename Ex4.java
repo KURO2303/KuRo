150580c15d32553d1bbf6fc062ed48b94730861f
Bùi Minh Đức <160729391+ducbui2005hadd@users.noreply.github.com>
2024-02-24 21:04:39 +0700

    My exercise 4 at Thinking In Java 4th Edition

---
+++ Ex4.java
@@ -0,0 +1,20 @@
+package com.mycompany.app.Week1_Code3;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            boolean Prime = true;
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0){
+                    Prime = false;
+                    break;
+                }
+            }
+            if(Prime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    
+}
+
