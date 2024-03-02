45784f75f267f68d5349d24ee623d7c9e2126d2c
ducbui2005hadd <23010513@st.phenikaa-uni.edu.vn>
2024-03-02 16:31:50 +0700

    Little change

--- src/main/java/com/mycompany/app/Week2_Code2/InitializationCleanupEx2.java
+++ src/main/java/com/mycompany/app/Week2_Code2/InitializationCleanupEx2.java
@@ -1,5 +1,10 @@
 package com.mycompany.app.Week2_Code2;
 
 public class InitializationCleanupEx2 {
-    
+    private char[] String;
+
+    public void UninitializationString(){
+        InitializationCleanupEx2 Ex2 = new InitializationCleanupEx2();
+        InitializationCleanupEx2 Duc2 = new InitializationCleanupEx2();
+    }  
 }
