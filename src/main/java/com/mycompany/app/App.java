899d7898a055fc02bf2b656d63f66b2d5588aded
ducbui2005hadd <23010513@st.phenikaa-uni.edu.vn>
2024-03-02 12:00:27 +0700

    app,java

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
+package com.mycompany.app;
+import com.mycompany.app.Week1_Code2.*;
+
+public class App {
+    public static void main(String[] args) {
+        Java_Each temp = new Java_Each();
+        temp.Duc4();
+}
+}
