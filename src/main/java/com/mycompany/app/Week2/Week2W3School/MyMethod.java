a5727bf18104191caa2af77a5ff789fbba49c276
KURO2303 <Redragon23030509@gmail.com>
2024-03-03 20:49:34 +0700

    Week 2 W3school

---
+++ src/main/java/com/mycompany/app/Week2/Week2W3School/MyMethod.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.Week2.Week2W3School;
+
+public class MyMethod {
+    static void myMethod() {
+      System.out.println("I just got executed!");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+    }
+  }
