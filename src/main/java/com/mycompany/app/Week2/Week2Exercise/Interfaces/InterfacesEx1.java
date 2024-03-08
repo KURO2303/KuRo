e0087d7361c9887df57b9c32de122a331dac66f3
KURO2303 <Redragon23030509@gmail.com>
2024-03-08 10:03:47 +0700

    fix again

--- src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx1.java
+++ src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx1.java
@@ -1,6 +1,6 @@
 package com.mycompany.app.Week2.Week2Exercise.Interfaces;
 
-abstract class Rodent {
+abstract class RodentEx7 {
 	@SuppressWarnings("unused")
 	private String name = "Rodent";
 	abstract protected void eat(); 
@@ -9,7 +9,7 @@ abstract class Rodent {
 	abstract public String toString(); 
 }
 
-class Mouse extends Rodent {
+class MouseEx7 extends RodentEx7 {
 	private String name = "Mouse";
 	protected void eat() { println("Mouse.eat()"); }
 	private void println(String string) {}
@@ -18,7 +18,7 @@ class Mouse extends Rodent {
 	public String toString() { return name; }
 }
 
-class Rat extends Rodent {
+class RatEx7 extends RodentEx7 {
 	private String name = "Rat";
 	protected void eat() { println("Rat.eat()"); }
 	private void println(String string) {}
@@ -27,7 +27,7 @@ class Rat extends Rodent {
 	public String toString() { return name; }
 }
 
-class Squirrel extends Rodent {
+class SquirrelEx7 extends RodentEx7 {
 	private String name = "Squirrel";
 	protected void eat() { println("Squirrel.eat()"); }
 	private void println(String string) {}
@@ -41,8 +41,8 @@ public class InterfacesEx1 {
     private static void println(String string) {
     }
 	public static void main(String[] args) {
-		Rodent[] rodents = new Rodent[10];
-		for(Rodent r : rodents) {
+		RodentEx7 rodents = new RodentEx7[];
+		for(RodentEx7 r : rodents) {
 			r = gen.next();
 			println(r + ": ");
 			r.eat();
