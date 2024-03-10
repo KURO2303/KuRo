ad57dde9cc1f8daedee31988f0f3c05abb84fc73
KURO2303 <Redragon23030509@gmail.com>
2024-03-10 21:03:19 +0700

    update (again)

--- src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx14.java
+++ src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx14.java
@@ -1,31 +1,31 @@
 package com.mycompany.app.Week2.Week2Exercise.Interfaces;
 
-interface History {
+interface History14 {
 	void u();
 	void v();
 }
 
-interface Exam {
+interface Exam14 {
 	void w();
 	void x();
 }
 
-interface Labs {
+interface Labs14 {
 	void y();
 	void z();
 }
 
-interface Workup extends History, Exam, Labs {
+interface Workup14 extends History14, Exam14, Labs14 {
 	void zz();
 }
 
-class Doctor {
+class Doctor14 {
 	@SuppressWarnings("unused")
 	private int i = 0;
 	public void doc() {}
 }
 
-class Anesthesiologist extends Doctor implements Workup {
+class Anesthesiologist14 extends Doctor14 implements Workup14 {
 	@SuppressWarnings("unused")
 	private int j = 0;
 	public void u() {}
@@ -38,12 +38,12 @@ class Anesthesiologist extends Doctor implements Workup {
 }
 
 public class InterfacesEx14 {
-	public static void m1(History history) { history.v(); }
-	public static void m2(Exam exam) { exam.w(); }
-	public static void m3(Labs labs) { labs.y(); } 
-	public static void m4(Workup workup) { workup.zz(); }
+	public static void m1(History14 history) { history.v(); }
+	public static void m2(Exam14 exam) { exam.w(); }
+	public static void m3(Labs14 labs) { labs.y(); } 
+	public static void m4(Workup14 workup) { workup.zz(); }
 	public static void main(String[] args) {
-		Anesthesiologist anes = new Anesthesiologist();
+		Anesthesiologist14 anes = new Anesthesiologist14();
 		m1(anes);
 		m2(anes);
 		m3(anes);
--- src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx15.java
+++ src/main/java/com/mycompany/app/Week2/Week2Exercise/Interfaces/InterfacesEx15.java
@@ -15,17 +15,17 @@ interface Labs15 {
 	void z();
 }
 
-interface Workup extends History15, Exam15, Labs15 {
+interface Workup15 extends History15, Exam15, Labs15 {
 	void zz();
 }
 
-abstract class Doctor {
+abstract class Doctor15 {
 	@SuppressWarnings("unused")
 	private int i = 0;
 	abstract void doc();
 }
 
-class Anesthesiologist extends Doctor implements Workup {
+class Anesthesiologist15 extends Doctor15 implements Workup15 {
 	@SuppressWarnings("unused")
 	private int j = 0;
 	public void u() {}
@@ -42,9 +42,9 @@ public class InterfacesEx15 {
 	public static void m1(History15 history) { history.v(); }
 	public static void m2(Exam15 exam) { exam.w(); }
 	public static void m3(Labs15 labs) { labs.y(); } 
-	public static void m4(Workup workup) { workup.zz(); }
+	public static void m4(Workup15 workup) { workup.zz(); }
 	public static void main(String[] args) {
-		Anesthesiologist anes = new Anesthesiologist();
+		Anesthesiologist15 anes = new Anesthesiologist15();
 		m1(anes);
 		m2(anes);
 		m3(anes);
