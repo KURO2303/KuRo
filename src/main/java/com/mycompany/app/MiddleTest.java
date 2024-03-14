0e15a51b5d2aa8f1897a78209431fc3b35ee7a12
KURO2303 <Redragon23030509@gmail.com>
2024-03-14 15:49:41 +0700

    Middle Test Update

--- src/main/java/com/mycompany/app/MiddleTest.java
+++ src/main/java/com/mycompany/app/MiddleTest.java
@@ -1,6 +1,6 @@
-package com.mycompany.app.Week3.Week3ClassExercise;
+package com.mycompany.app;
 
-class Note{
+class Note1{
     public static String a = "A";
     public static String b = "B";
     public static String c = "MIDDLE_C";
@@ -9,62 +9,62 @@ class Note{
     public static String f = "F";
     public static String g = "G";
 }
-abstract class Instrument {
+abstract class Instrument1 {
     abstract void play(String n);
     String what() { 
         return "Instrument"; 
     }
     abstract void adjust();
    }
-   class Wind extends Instrument {
+   class Wind1 extends Instrument1 {
     public void play(String n) {
     System.out.println("Play wind instrument in " + n);
     }
     public String what() { return "Wind"; }
     public void adjust() {}
    }
-   class Percussion extends Instrument {
+   class Percussion1 extends Instrument1 {
     public void play(String n) {
         System.out.println("Play percussion instrument in " + n);
     }
     public String what() { return "Percussion"; }
     public void adjust() {}
    }
-   class Stringed extends Instrument {
+   class Stringed1 extends Instrument1 {
     public void play(String n) {
         System.out.println("Play stringed instrument in " + n);
     }
     public String what() { return "Stringed"; }
     public void adjust() {}
    }
-   class Brass extends Wind {
+   class Brass1 extends Wind1 {
     public void play(String n) {
         System.out.println("Play brass instrument in " + n);
     }
     public void adjust() { System.out.println("Brass.adjusted"); 
     }
    }
-   class Woodwind extends Wind {
+   class Woodwind1 extends Wind1 {
     public void play(String n) {
         System.out.println("Play woodwind instrument in " + n);
     }
     public String what() { return "Woodwind"; }
    }
-   public class ExerciseMusic {
-    static void tune(Instrument i) {
-    i.play(Note.c);
+   public class MiddleTest {
+    static void tune(Instrument1 i) {
+    i.play(Note1.c);
     }
-    static void tuneAll(Instrument[] I) {
-    for(Instrument i : I)
+    static void tuneAll(Instrument1[] I) {
+    for(Instrument1 i : I)
     tune(i);
     }
     public static void main(String[] args) {
-    Instrument[] orchestra = {
-    new Wind(),
-    new Percussion(),
-    new Stringed(),
-    new Brass(),
-    new Woodwind()
+    Instrument1[] orchestra = {
+    new Wind1(),
+    new Percussion1(),
+    new Stringed1(),
+    new Brass1(),
+    new Woodwind1()
     };
     tuneAll(orchestra);
  }
